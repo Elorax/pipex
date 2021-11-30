6318c3a29e844b386a9ca8058ce0bef4297e7d56
Antoine BIERSOHN <abiersoh@e1r5p16.42.fr>
2021-12-01 00:36:25 +0100

    osef

--- pipex.c
+++ pipex.c
@@ -38,28 +38,82 @@ void	pipex(int f1, int f2, char *cmd1, char *cmd2)
 
 }
 
+static char	*ft_strjoin(char *s1, char *s2);
+{
+	char	*dest;
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dest)
+		return (NULL);
+	while (*s1)
+		*dest++ = *s1++;
+	while (*s2)
+		*dest++ = *s2++;
+	*dest = 0;
+	return (dest);
+}
 
 
+void	child_process(int f1, char **av, char **env)
+{
+	char	**path;
 
+	path = ft_split(env[13], ':');
+	cmds = ft_split(av[2], ' ');
+	i = -1;
+	while(mypaths[++i])
+	{
+		cmd1 = ft_strjoin(mypaths);
+
+	}
+}
+
+int pipex(int f1, int f2, char **av, char **env)
+{
+	char	**path;
+	char	**cmds;
+	char	*cmd1;
+	int		i;
+	int		end[2];
+	pid_t	parent;
+
+	parent = fork();
+	if (!parent)
+
+	pipe(end);
+	path = ft_split(env[13], ':');
+	//PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/usr/local/munki
+	cmds = ft_split(av[2], ' ');
+	i = -1;
+	while (mypaths[++i])
+	{
+		cmd1 = ft_strjoin(my_paths[i], av[2]);
+		if (!cmd1)
+			return (-42);
+		execve(cmd, mycmdargs, env);
+		//perror("Error calling execve");
+		free(cmd);
+	}
+	return (EXIT_FAILURE);
+}
+
+typedef struct t_
 int main(int ac, char **av, char **env)
 {
+	int		fd1;
+	int		fd2;
+
 	if (ac != 5)
 	{
 		printf("Mauvais nombre d'arguments\n");
 		return 0;
-	/
-	
-	char	**cmd1 = ft_split(av[2], ' ');
-	char	**cmd2 = ft_split(av[3], ' ');
-	int	fd1 = open(argv[1], O_RDONLY);
+	}
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 		return(perror("Open : "));
-	int fd2 = open(argv[4], O_WRONLY);
+	fd2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fd2 == -1)
 		return(perror("Open : "));
-	char	*path = envp[8];
-
-
-
-
+	pipex(f1, f2, ag, envp);
+	close(f1);
+	close(f2);
 }
